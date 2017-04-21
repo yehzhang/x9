61f0ac66b32480e65e8ea95f8e49f9c8e2de06b9
yehzhang <simonzhangintp@gmail.com>
2017-04-20 23:37:15 -0700

    Handle load and store of memory

--- interpreter/environment.py
+++ interpreter/environment.py
@@ -21,35 +21,25 @@ class Registers:
         assert 0 <= config['reg_default_value'] <= 0xff
 
         super().__setattr__('names', config['reg_names'])
-        super().__setattr__('registers', {k: config['reg_default_value']
-                                          for k in config['reg_names']})
-        super().__setattr__('max_val', config['reg_max_value'])
+        registers = {k: Byte().set(config['reg_default_value'])
+                     for k in config['reg_names']}
+        super().__setattr__('registers', registers)
 
     def __str__(self):
-        regs_str = '\n'.join('\t{}: 0x{:02x}'.format(n, self.registers[n])
+        regs_str = '\n'.join('\t{}: {}'.format(n, str(self.registers[n]))
                              for n in self.names)
         return 'registers: \n{}'.format(regs_str)
 
     def __getattr__(self, name):
-        return self.registers[name]
+        return self.registers[name].get()
 
     def __setattr__(self, name, value):
-        # TODO are registers signed?
-        assert 0 <= value <= self.max_val
-
-        self.registers[name] = value
+        self.registers[name].set(value)
 
     def __getitem__(self, key):
         if isinstance(key, int):
             key = self.names[key]
-        return self.registers[key]
-
-
-def make_byte_list(size, default=None):
-    """
-    :param default: either an int or a list
-    """
-    raise NotImplementedError
+        return self.__getattr__(key)
 
 
 class Memory:
@@ -60,12 +50,17 @@ class Memory:
         assert 0 <= config['mem_default_value'] <= 0xff
         assert 0 < config['mem_size']
 
-        self.memory = [config['mem_default_value']] * config['mem_size']
+        self.memory = [Byte().set(config['mem_default_value'])
+                       for _ in range(config['mem_size'])]
+
+    def __str__(self):
+        return '\n'.join(' '.join(map(str, self.memory[i:i+8]))
+                         for i in range(0, len(self.memory), 8))
 
     def __getitem__(self, key):
         """
-        Load a byte at address 0xff: memory[0xff]
-        Load a word at address 0xff: memory[0xff, 4]
+        Load an unsigned byte at address 0xff: memory[0xff]
+        Load an unsigned word at address 0xff: memory[0xff, 4]
         """
         if not isinstance(key, tuple):
             key = key,
@@ -83,22 +78,57 @@ class Memory:
             key = key, value
         self.store(*key)
 
-    def load(self, addr, size=1):
+    def load(self, addr, size=1, signed=False):
         assert 1 <= size
         assert 0 <= addr
         assert addr + size <= len(self.memory)
 
-        num = 0
-        for byte in self.memory[addr:addr+size]:
-            num = (num << 8) | byte
-        return num
+        value = 0
+        for i in range(addr, addr + size):
+            byte = self.memory[i].get()
+            value = (value << 8) | byte
+
+        # Convert to signed integer
+        if signed:
+            msb_mask = 1 << (size * 8 - 1)
+            if value & msb_mask:  # if negative
+                max_value = 2 ** (size * 8)
+                value -= max_value
+
+        return value
 
     def store(self, addr, value, size=1):
-        """ MSBs are discarded if `value' cannot be contained in `size' bytes """
         assert 1 <= size
         assert 0 <= addr
         assert addr + size <= len(self.memory)
 
+        value = convert_to_unsigned_integer(value, size)
         for i in range(size - 1, -1, -1):
-            self.memory[addr + i] = value & 0xff
+            self.memory[addr + i].set(value & 0xff)
             value >>= 8
+
+
+class Byte:
+
+    def __init__(self):
+        self.value = 0
+
+    def __str__(self):
+        return '0x{:02x}'.format(self.value)
+
+    def set(self, value):
+        # Signed minimum and unsigned maximum
+        self.value = convert_to_unsigned_integer(value, 1)
+        return self
+
+    def get(self):
+        return self.value
+
+
+def convert_to_unsigned_integer(value, size):
+    max_value = 2 ** (size * 8)
+    if not (-max_value // 2 <= value < max_value):
+        msg = '{} is out of range of {} bytes'.format(value, size)
+        raise ValueError(msg)
+    all_f_mask = max_value - 1
+    return value & all_f_mask
---
+++ tests/test_memory.py
@@ -0,0 +1,45 @@
+import pytest
+import sys
+import os
+
+
+def test_answer():
+    sys.path.append(os.path.abspath(os.path.join(
+        os.path.dirname(__file__), os.path.pardir)))
+    from interpreter.environment import Memory
+
+    mem = Memory({
+        'mem_default_value': 0,
+        'mem_size': 64,
+    })
+
+    mem[1, 2] = 0xf000
+    assert mem[0, 3] == 0xf000
+    assert mem.load(0, 3, True) == 0xf000
+    assert mem.load(0, 2, True) == 0xf0
+    assert mem.load(1, 1, False) == 0xf0
+    assert mem.load(1, 1, True) == -16
+
+    mem[0, 4] = 123
+    assert mem[0, 4] == 123
+    assert mem.load(0, 4, True) == 123
+
+    mem[0, 4] = -123
+    assert mem[0, 4] == 4294967173
+    assert mem.load(0, 4, True) == -123
+    assert mem.load(0, 5, True) == -31488
+    assert mem.load(1, 5, True) == -8060928
+    assert mem.load(1, 3, True) == -123
+    assert mem.load(2, 2, True) == -123
+    assert mem.load(3, 1, True) == -123
+    assert mem.load(0, 3, True) == -1
+    assert mem.load(0, 2, True) == -1
+    assert mem.load(0, 1, True) == -1
+
+    mem[0, 4] = -256
+    assert mem.load(0, 3, True) == -1
+    assert mem.load(3, 2, True) == 0
+
+    with pytest.raises(ValueError):
+        mem[0, 2] = 0x10000
+    assert mem[0, 4] == 4294967040
