fa0ca4f5bf60f08b67160a79d37eec7dc99ace96
yehzhang <simonzhangintp@gmail.com>
2017-05-06 15:17:24 -0700

    Add ALU

--- verilog modules/ALU.sv
+++ verilog modules/ALU.sv
@@ -1,65 +1,42 @@
-// Engineer: 
-// 
+// Engineer:
+//
 // Create Date:    2016.10.15
-// Design Name: 
-// Module Name:    ALU 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Design Name:
+// Module Name:    ALU
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Dependencies: 
+// Dependencies:
 //
-// Revision: 
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //   combinational (unclocked) ALU
 
 import definitions::*;			  // includes package "definitions"
 module ALU(
-  input        SC_IN,             // shift in/carry in 
-  input [ 3:0] OP,				  // ALU opcode, part of microcode
-  input [ 7:0] INPUTA,			  // data inputs
-               INPUTB,
-  output logic [7:0] OUT,		  // or:  output reg [7:0] OUT,
-  output logic SC_OUT			  // shift out/carry out
+  input        cin,             // carry in
+  input [3:0]  ctrl_input,				  // ALU opcode, part of microcode
+  input [7:0]  a,			  // data inputs
+               b,
+  output logic [7:0] out,		  // or:  output reg [7:0] OUT,
+  output logic cout			  // carry out
     );
-	 
-  op_mne op_mnemonic;			  // type enum: used for convenient waveform viewing
-	
+
   always_comb begin
-// option 1 -- single instruction for both LSW & MSW
-	case(OP[3:2])
-	  kADD : case(OP[1:0])
-	    00: {SC_OUT,INPUTA} << 1;
-		01: (INPUTA << 1) + SC_IN;
-	  
-	  {SC_OUT,OUT} = INPUTA + INPUTB + SC_IN;    // universal add operation
-	  kLSA : {SC_OUT,OUT} = (INPUTA<<1) + SC_IN;  	// universal shift instruction
-//	  kXOR : OUT = INPUTA^INPUTB;
-	  default: {SC_OUT,OUT} = 0;
-	endcase
-// option 2 -- separate LSW and MSW instructions
-    case(OP)
-	  kADDL : {SC_OUT,OUT} = INPUTA + INPUTB ;    // universal add operation
-	  kLSAL : {SC_OUT,OUT} = (INPUTA<<1) ;  	// universal shift instruction
-	  kADDU : begin
-	            OUT = INPUTA + INPUTB + SC_IN;    // universal add operation
-                SC_OUT = 0;   
-              end
-	  kLSAU : begin
-	            OUT = (INPUTA<<1) + SC_IN;  	// universal shift instruction
-                SC_OUT = 0;
-               end
-      kXOR  : OUT = INPUTA ^ INPUTB;
-	  kBRNE : OUT = INPUTA - INPUTB;   // use in conjunction w/ instruction decode 
+    cout = 0;
+    case (ctrl_input)
+        kAdd:  {cout, out} = a + b;
+        kAddC: {cout, out} = a + b + cin;
+        kSub:  out = a - b;
+        kSll:  out = a >> b;
+        kSra:  out = a >>> b;
+        kAnd:  out = a & b;
+        kOr:   out = a | b;
+        kNeg:  out = ~a;
+        default: out = 0;
     endcase
-	case(OUT)
-	  16'b0 :   ZERO = 1'b1;
-	  default : ZERO = 1'b0;
-	endcase
-//$display("ALU Out %d \n",OUT);
-    op_mnemonic = op_mne'(OP);
-  end
 
 endmodule
--- verilog modules/definitions.sv
+++ verilog modules/definitions.sv
@@ -1,19 +1,27 @@
 //This file defines the parameters used in the alu
 package definitions;
-    
+
 // Instruction map
-    const logic [2:0] kADDL  = 'b000;
-	const logic [2:0] kADDU  = 'b001;
-    const logic [2:0] kLSAL  = 'b010;
-	const logic [2:0] kLSAU  = 'b011;
-    const logic [1:0]kAND  = 2'b10;
-    const logic [1:0]kXOR  = 2'b11;
-    
-    typedef enum logic[1:0] {
-        ADDU    = 2'h0, 
-        SUBU    = 2'h1, 
-        AND     = 2'h2,
-        XOR     = 2'h3
-    } op_mne;
-    
+    const logic [2:0] kAdd  = 3'b000;
+    const logic [2:0] kAddC = 3'b001;
+    const logic [2:0] kSub  = 3'b010;
+    const logic [2:0] kSll  = 3'b011;
+    const logic [2:0] kSra  = 3'b100;
+    const logic [2:0] kOr   = 3'b100;
+    const logic [2:0] kNeg  = 3'b101;
+    const logic [2:0] kAnd  = 3'b111;
+
+    // typedef enum logic[3:0] {
+    //     kAdd  = 4'b0000;
+    //     kAddC = 4'b0001;
+    //     kSub  = 4'b0010;
+    //     kSll  = 4'b0011;
+    //     kSra  = 4'b0100;
+    //     kSllC = 4'b0101;
+    //     kSrlC = 4'b0110;
+    //     kAnd  = 4'b0111;
+    //     kOr   = 4'b1000;
+    //     kNeg  = 4'b1001;
+    // } alu_ctrl;
+
 endpackage // defintions
