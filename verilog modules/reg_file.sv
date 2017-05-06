cbf4204fafa482eb43e587785e453047424c7814
diqian <qiandi.eric@gmail.com>
2017-05-06 13:50:31 -0700

    changed file 1

--- verilog modules/reg_file.sv
+++ verilog modules/reg_file.sv
@@ -1,6 +1,6 @@
 module reg_file #(parameter W=8, D=4)(
   input           clk,
-                  write_en,
+                  ctrl_reg_write, //control reg write 
   input  [ D-1:0] raddrA,
                   raddrB,
                   waddr,
@@ -13,12 +13,12 @@ module reg_file #(parameter W=8, D=4)(
 logic [W-1:0] registers[2**D];	  // or just registers[16] if we know D=4 always
 
 // combinational reads w/ blanking of address 0
-assign      data_outA = raddrA? registers[raddrA] : '0;	   // can't read from addr 0, just like MIPS
-always_comb data_outB = raddrB? registers[raddrB] : 'b0;
+assign      data_outA = registers[raddrA];
+always_comb data_outB = registers[raddrB];
 
 // sequential (clocked) writes	(likewise, can't write to addr 0)
 always_ff @ (posedge clk)
-  if (write_en && waddr)
+  if (ctrl_reg_write)
     registers[waddr] <= data_in;
 
 endmodule
