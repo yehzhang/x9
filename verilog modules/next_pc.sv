6ff066bc88710e7404b768ac191f55be73644b73
yehzhang <simonzhangintp@gmail.com>
2017-05-06 21:18:51 -0700

    Rename PC to next_pc

--- verilog modules/next_pc.sv
+++ verilog modules/next_pc.sv
@@ -1,20 +1,20 @@
-module PC #(parameter INST_WIDTH=9, OV=31)(
+module next_pc #(parameter INST_WIDTH=9, OV=31)(
     input clk,
     input reset,
     input ctrl_branch,
-    input alu_zero,
-    input [INST_WIDTH-1:0] inst_addr_in,
+    input take_branch,
+    input [INST_WIDTH-1:0] inst_addr_in, // used by branch or reset
     output logic[INST_WIDTH-1:0] inst_addr_out,
     output logic halt
     );
 
     always_ff @(posedge clk) begin
         if(reset) begin
-            inst_addr_out <= 0;
+            inst_addr_out <= inst_addr_in;
             halt <= 0;
         end else begin
             if(ctrl_branch) begin
-                if(alu_zero) begin
+                if(take_branch) begin
                     inst_addr_out = inst_addr_in;
                 end
             end else begin
