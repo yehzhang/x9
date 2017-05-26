4c8f23080c93c3ad852e8b9bd5d2d95133220427
yehzhang <simonzhangintp@gmail.com>
2017-05-26 01:11:44 -0700

    Update

--- interpreter/setup.py
+++ interpreter/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='x9-interpreter',
     version='0.1',
-    author='Simon Zhang',
+    author=['Simon Zhang', 'Di Qian', 'Jiayi Xu'],
     license='MIT',
     packages=find_packages(),
     install_requires=[],
