66b4ea25c2bd9a579b2f17b77ad2527d692606c2
diqian <qiandi.eric@gmail.com>
2017-06-16 06:17:08 -0700

    changed div_x9

--- targets/div_x9.s
+++ targets/div_x9.s
@@ -30,14 +30,14 @@
 # THIRD_ELSE
 
 define temp                 r3
-define temp2                r4      #(shift_MSB),always 0
-define temp3                r5      #(shift_LSB)
+define temp2                r4      
+define temp3                r5      #(shift)
 define quotient_MSW         r6      #(set)
 define quotient_LSW         r7      #(set)
 define div_MSW              r8      #(set)
 define div_LSW              r9      #(set)
-define divident_neg         r10     #(set)
-define divisor_neg          r11     #(set)
+define temp4          r10     #(set)
+define temp5          r11     #(set)
 define divident_temp_MSW    r12     #(set)
 define divident_temp_LSW    r13     #(set)
 define divisor_temp         r14     #(set)
@@ -69,65 +69,19 @@ START:
     add div_MSW
     add div_LSW
 
-    #if(dividend >> 15 == 1){
-    lw r0, DIVIDEND_ADDR_MSW # get most sig byte and shift by 7
-    set r1, SEVEN
-    srl r0
-    set r1, 1
-    bne FIRST_ELSE
-    # divident_neg = 1;
-    set r1, 1
-    set r0, 0
-    add divident_neg
-    # divident_temp = ~dividend + 1;
-    lw r0, DIVIDEND_ADDR_LSW
-    neg r1
-    lw r0, DIVIDEND_ADDR_MSW
-    neg r2
-    set r0, 1
-    add divident_temp_LSW
-    set r0, 0
-    mov r1, r2
-    adc divident_temp_MSW
-FIRST_ELSE:
-    # divident_neg = 0;
-    set r0, 0 # good
-    set r1, 0
-    add divident_neg
     # divident_temp = dividend;
     lw r0, DIVIDEND_ADDR_LSW
     set r1, 0
     add divident_temp_LSW
     lw r0, DIVIDEND_ADDR_MSW
-    add divident_temp_MSW  # good
+    add divident_temp_MSW  
 
-    #second if
-    # if(divisor >> 7 == 1){
-    lw r0, DIVISOR_ADDR
-    set r1, SEVEN
-    srl r1
-    set r0, 1
-    bne SECOND_ELSE
-    # divisor_neg = 1;
-    set r0, 0
-    set r1, 1
-    add divisor_neg
-    # divisor_temp = ~divisor + 1;
-    lw r0, DIVISOR_ADDR
-    neg r1
-    set r0, 1
-    add divisor_temp
-SECOND_ELSE:
-    # divisor_neg = 0;
-    set r0, 0
-    set r1, 0
-    add divisor_neg
     # divisor_temp = divisor;
     lw r1, DIVISOR_ADDR
     set r0, 0
     add divisor_temp
 
-    # int i = 0;
+    # int i = -1;
     set r0, 0
     set r1, 1
     sub r0
@@ -135,7 +89,7 @@ SECOND_ELSE:
     add i
 
 FOR_LOOP_START:
-    # i++
+    # i++ # i become 0 when first run
     mov r0, i
     set r1, 1
     add i
@@ -143,10 +97,8 @@ FOR_LOOP_START:
     # int shift = 15 - i;
     set r0, FIFTEEN
     mov r1, i
-    sub temp3 # shift_LSB   # good
-    set r0, 0
-    set r1, 0
-    add temp2 # shift_MSB    # good
+    sub temp3 # shift
+
     # shift = divident_temp >> shift;
     	mov r0, divident_temp_LSW
  	mov r1, temp3
@@ -162,13 +114,12 @@ FOR_LOOP_START:
  	sra r0
 
 
+
     # shift = shift & 1;
     mov r1, r2
     set r0, 1
     and temp3
-    set r0, 0
-    set r1, 0
-    add temp2
+
     # int div = div << 1;
     set r0, 1
     set r1, 0
@@ -189,7 +140,6 @@ FOR_LOOP_START:
 
 
 
-
     # div = div + shift;
     mov r0, temp3
     mov r1, div_LSW
@@ -198,30 +148,45 @@ FOR_LOOP_START:
     mov r1, div_MSW
     adc div_MSW
 
+    # quotient = (quotient << 1);
+    set r0, 1
+    set r1, 0
+    add r3
+    	mov r0, quotient_MSW
+ 	mov r1, r3
+ 	sll r2
+ 	set r0, 8
+ 	sub r1
+ 	mov r0, quotient_LSW
+ 	srl r1
+ 	mov r0, r2
+ 	or quotient_MSW
+ 	mov r0, quotient_LSW
+ 	mov r1, r3
+ 	sll quotient_LSW
 
-    # if(div >= divisor_temp){ # third if
-    # r3,r4,r5 are free to use now
-    # compare upper MSB with 0
+
+
+
+    # if(div >= divisor_temp){  // check div < divisor_temp
+    # compare upper MSB with 0, if MSB < 0, then it's negative
+    
     mov r0, div_MSW
     set r1, 0
-    blts THIRD_ELSE
-    # if MSB is same，both 0,compare LSB
+    blts CHECK_I
+    # if MSB is same，means both 0, compare LSB, if LSB < divisor_temp
     mov r0, div_LSW
     mov r1, divisor_temp
-    blts THIRD_ELSE
+    blts CHECK_I # if we didn't branch, then div >= divisor_temp
     # div = div - divisor_temp;
     # first: extend bit for divisor_temp
     set r0, 0
     set r1, 0
     add temp2 # temp2 holds MSB for extend_divisor_temp
-    set r0, 0
-    mov r1, divisor_temp # LSB
-    beq EXTEND_BIT_COMPELET
-    blt EXTEND_BIT_COMPELET
-    set r0, 0
-    neg temp2
-EXTEND_BIT_COMPELET:
-    # negate temp2, and divisor_temp into temp2, temp3
+              # remember, divisor_temp is just divisor which is positive
+              # thus, temp2 is always 0
+
+    # negate temp2, and divisor_temp into temp2(MSB), temp3(LSB)
     # then add 1 to both
     mov r0, divisor_temp
     neg temp3
@@ -233,35 +198,13 @@ EXTEND_BIT_COMPELET:
     mov r0, temp2
     set r1, 0
     adc temp2
-    # finally, div = div + (- divisor_temp);
+    # finally, div = div + (- divisor_temp); = div + (temp2, temp3)
     mov r0, div_LSW
-    mov r1, temp3
+    mov r1, temp3 
     add div_LSW
     mov r1, div_MSW
-    mov r1, temp2
+    mov r0, temp2
     adc div_MSW
-    # quotient = (quotient << 1);
-    set r0, 1
-    set r1, 0
-    add r3
-    # set r1, 28 #NOP
-    	mov r0, quotient_MSW
- 	mov r1, r3
- 	sll r2
- 	set r0, 8
- 	sub r1
- 	mov r0, quotient_LSW
- 	srl r1
- 	mov r0, r2
- 	or quotient_MSW
- 	mov r0, quotient_LSW
- 	mov r1, r3
- 	sll quotient_LSW
-
-
-
-
-    # set r1, 28 #NOP
     # quotient = quotient + 1;
     mov r0, quotient_LSW
     set r1, 1
@@ -270,54 +213,13 @@ EXTEND_BIT_COMPELET:
     set r1, 0
     adc quotient_MSW
 
-    # set r1, 28 #NOP
-
-    set r0, 0
-    set r1, 0
-    beq CHECK_I
-
-THIRD_ELSE:
-    # quotient = (quotient << 1);
-    set r0, 1
-    set r1, 0
-    add r3
-    	mov r0, quotient_MSW
- 	mov r1, r3
- 	sll r2
- 	set r0, 8
- 	sub r1
- 	mov r0, quotient_LSW
- 	srl r1
- 	mov r0, r2
- 	or quotient_MSW
- 	mov r0, quotient_LSW
- 	mov r1, r3
- 	sll quotient_LSW
-
-
-
-
 CHECK_I:
     # time to check i
     set r0, FIFTEEN
     mov r1, i
     bne FOR_LOOP_START
+    # end of for loop  
 
-    # end of for loop
-    # if(divident_neg != divisor_neg){
-    mov r0, divident_neg
-    mov r1, divisor_neg
-    beq IF_EXIT
-    # quotient = ~quotient + 1
-    mov r0, quotient_LSW
-    neg r1
-    mov r0, quotient_MSW
-    neg r2
-    set r0, 1
-    add quotient_LSW
-    set r0, 0
-    mov r1, r2
-    adc quotient_MSW
 IF_EXIT:
     # store back into 126(MSW), 127(LSW)
     mov r0, quotient_MSW
