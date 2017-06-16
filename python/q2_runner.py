fb3f03bc046cc34ba50c2b37cec1d9f735bf44d9
yehzhang <simonzhangintp@gmail.com>
2017-06-16 03:09:23 -0700

    Add test cases

--- python/q2_runner.py
+++ python/q2_runner.py
@@ -9,8 +9,8 @@ class Debugger(Callback):
     def on_instruction_begin(self, inst, env):
         text = str(inst)
         print(text)
-        if text in ('blt for_entry:', 'sw r0, RETURN_ADDR'):
-            print(env)
+        if text in ('blt for_entry:'):
+            # print(env)
             pass
         return
 
@@ -19,10 +19,19 @@ class Debugger(Callback):
 
 
 def main():
+    # Test Case 1
+    # s = '01001' * 102 + '00'
+    # pattern = 0b1001
+
+    # Test Case 3
+    s = '1' * 64 * 8
+    pattern = 0b1111
+
     mem_default = [0] * 256
-    string = [0b01001010, 0b10010100, 0b00010001, 0b01010000]
+    string = [int(s[i*8:(i + 1)*8], 2) for i in range(len(s) // 8)]
+    print(', '.join('{:08b}'.format(s) for s in string))
     mem_default[32:32+len(string)] = string
-    mem_default[9] = 0b0101  # pattern
+    mem_default[9] = pattern
     mem_default[255] = 255  # max_int
     config = {
        'reg_default': 0,
@@ -31,7 +40,9 @@ def main():
     cbs = [
         Debugger(),
     ]
-    Interpreter(config, cbs).load('../targets/string_match_x9.s').run()
+    env = Interpreter(config, cbs).load('../targets/string_match_x9.s').run()
+    print(env)
+    print('Result count', env.memory[10])
 
 if __name__ == '__main__':
     main()
