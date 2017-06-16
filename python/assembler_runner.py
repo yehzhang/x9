0ebf94e416039b3cac3c05935de9c84909afba27
Jiayi <xjyjiayi@gmail.com>
2017-06-16 05:14:39 -0700

    cc

--- python/assembler_runner.py
+++ python/assembler_runner.py
@@ -2,7 +2,7 @@ from interpreter import Assembler
 import os.path
 
 def main():
-    Assembler().load('../targets/all.s').run('~/Downloads/output')
+    Assembler().load('../targets/cordic_x9.s').run('~/Desktop/machine_code')
 
 if __name__ == '__main__':
     main()
